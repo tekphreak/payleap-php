697f61004d2fed7ef6b1f7a559ce753bb3290b7b
tekphreak <michaelrhogan@gmail.com>
2016-11-01 10:37:00 -0400

    Update ProcessCreditCard.php

--- ProcessCreditCard.php
+++ ProcessCreditCard.php
@@ -1,26 +1,26 @@
 <?php 
  
 $post_body = http_build_query(array(
-'UserName'   => 'username_API',    //your username_API goes here. 
-'Password'   => 'yourpassword',    //Your password from your payleap account 
+'UserName'   => 'username_API',      //your username_API goes here. 
+'Password'   => 'yourpassword',      //Your password from your payleap account 
 'TransType'  => 'Auth',
-'CardNum'    => '4111111111111111', //Credit Card Number
-'ExpDate'    => '1222',             //Must Be Future Date
+'CardNum'    => '4111111111111111',  //Credit Card Number
+'ExpDate'    => '1222',              //Must Be Future Date
 'NameOnCard' => 'Smith',
 'Amount'     => 10.00,
 'CVNum'      =>'789',
 'Zip'        => '12345',
-'PNRef'      => '', //Sending the PNRef name pair is required.
+'PNRef'      => '',                  //Sending the PNRef name pair is required.
 ));
 
 /* See http://payleap.com/pdf/PayLeap-API-Guide-v2-3.pdf#page=17 for all required fields */
  
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL,'https://uat.payleap.com/transactservices.svc/ProcessCreditCard');
-curl_setopt($curl, CURLOPT_POST, true); // Use POST
-curl_setopt($curl, CURLOPT_POSTFIELDS, $post_body); // Setup post body
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // Receive server response
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // Receive server response
+curl_setopt($curl, CURLOPT_POST, true);               // Use POST
+curl_setopt($curl, CURLOPT_POSTFIELDS, $post_body);   // Setup post body
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);        // Receive server response
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);    // Receive server response
 curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
 // Execute request and read response
 $data = curl_exec($curl);
